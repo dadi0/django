96cde246610192315d79981acfc8576e28c6fe22
dadi <1442374919@qq.com>
2020-02-19 11:14:31 +0800

    update

--- statistical_run_time_decorator.py
+++ statistical_run_time_decorator.py
@@ -2,18 +2,18 @@ import datetime
 
 
 def count_time(func):
-    def int_time(*args, **kwargs):
+    def run_time():
         start_time = datetime.datetime.now()  # 程序开始时间
         func()
         over_time = datetime.datetime.now()   # 程序结束时间
         total_time = (over_time-start_time).total_seconds()
         print('程序共计%s秒' % total_time)
-    return int_time
+    return run_time
 
 
 @count_time
 def main():
-    print('>>>>开始计算函数运行时间')
+    print('开始')
     for i in range(1, 100):
         for j in range(i):
             print(j)
